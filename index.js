80ed229bb070bef919baed24b53ff241972ef6b8
Ayoola Taiwo <tayoola13@yahoo.com>
2022-01-11 01:05:22 +0100

    Changed pdf format to A# in the index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const { pdf } = require("./puppeteer_pdf/pdf");
 const { screenshot } = require("./puppeteer_screenshot/screenshot");
 
 // screenshot('https://fb.com', 'facebook-shot');
-pdf('https://fb.com', 'downloaded_file', 'A4');
+pdf('https://fb.com', 'downloaded_file', 'A3');
